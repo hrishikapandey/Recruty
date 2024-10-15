d37ebadf0dca10973dda5388a921709143c70499
hrishikapandey <hrishiap38@gmail.com>
2024-10-15 18:22:38 +0530

    backend completed

--- routes/job.js
+++ routes/job.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const {Job} = require("../schema/job.schema")
 const authMiddleware = require("../middleware/auth");
 const isAuth = require("../utils/index");
+const {z} =  require("zod");
+const {validateRequest} = require('zod-express-middleware');
 
 router.post("/", authMiddleware, async (req,res)=>{
     try{
@@ -37,7 +39,18 @@ router.get("/:id", authMiddleware, async (req,res)=>{
     res.status(200).json(job);
 })
 
-
+// router.get("/:id" , validateRequest({
+//     params: z.object({
+//         id: z.string().uuid()
+//     }),
+// }), authMiddleware, async (req,res) =>{
+//     const {id} = req.params;
+//     const job = await Job.findById(id);
+//     if(!job){
+//         return res.status(404).json({message : "Job not found"})
+//     }
+//     res.status(200).json(job);
+// })
 
 
 
